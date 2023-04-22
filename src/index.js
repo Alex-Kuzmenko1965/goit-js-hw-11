facc5a4ec6e9332ff412023aba26bc388c6a680e
Alex-Kuzmenko1965 <gc-dn@ukr.net>
2023-04-22 13:26:19 +0300

    Alexx

--- src/index.js
+++ src/index.js
@@ -1,89 +1,64 @@
 import './css/styles.css';
 import Notiflix from 'notiflix';
-import debounce from 'lodash.debounce';
+import SimpleLightbox from "simplelightbox";
 
-const DEBOUNCE_DELAY = 300;
-
-import { fetchCountries } from "./js/fetchCountries.js";
+import { fetchPixabay } from "./js/fetchPixabay.js";
 
 const refs = {
-  inputEvent: document.getElementById('search-box'),
-  countryList: document.querySelector('.country-list'),  
-  countryInfo: document.querySelector('.country-info'),
+  form: document.getElementById('search-form'),
+  btn: document.querySelector('button'),
+  galleryList: document.querySelector('.gallery'),
 };
-console.log(refs.inputEvent);
-console.log(refs.countryList);
-console.log(refs.countryInfo);
+console.log(refs.form);
+console.log(refs.btn);
+console.log(refs.galleryList);
 
-refs.inputEvent.addEventListener("input", debounce(onEventInput, DEBOUNCE_DELAY, {
-  'leading': true,
-  'trailing': false
-}));
-// refs.inputEvent.addEventListener("input", onEventInput);
+refs.form.addEventListener("submit", onSubmit);
 
-function onEventInput(e) {  
-  // console.log(e.currentTarget.value);
-  // console.log((e.currentTarget.value).trim());
-  const inputValue = (e.currentTarget.value).trim();
-  console.log(inputValue); 
-  fetchCountries(inputValue)
-    .then((data) => {
-      // console.log(data);        
-      if (data.length === 1) {
-        const markup = data.reduce(
-          (markup, data) => markup + createMarkupInfo(data), "");
+async function onSubmit(e) {  
+  e.preventDefault();  
+  const query = e.currentTarget.searchQuery.value;  
+  console.log(query);
+  // clearGalleryList();  
+  const { hits } = await fetchPixabay(query);
+  console.log(hits);  
+  try {             
+      if (hits.length) {
+        const markup = hits.reduce(
+          (markup, hits) => markup + createMarkupList(hits), "");
         console.log(markup);
-        updateNewsInfo(markup);
-      } else 
-      if (data.length > 10) {
-        Notiflix.Notify.info('Too many matches found. Please enter a more specific name');
-        refs.countryList.innerHTML = "";
-        refs.countryInfo.innerHTML = "";
-        return;
-      } else {
-        refs.countryList.setAttribute('style', 'padding: 0');       
-        const markup = data.reduce(
-          (markup, data) => markup + createMarkupList(data), "");
-        console.log(markup);
-        updateNewsList(markup);}      
-    })
-    // .catch((err) => onError(err))
-    .catch(onError);    
-}
+        updateGalleryList(markup);
+      }  
+    } catch (error) {onError()}  
+  }
 
-function createMarkupInfo({ name, capital, population,flags, languages }) {  
-  // console.log(Object.values(languages));  
-  // console.log((Object.values(languages)).join(' '));
+function createMarkupList({ webformatURL, largeImageURL,tags, likes, views, comments, downloads }) {  
   return `
-    <ul style="display: flex; align-items: baseline; list-style: none; margin: 0; padding: 0; gap: 10px">
-      <li><img src=${flags.svg || "No flags"} width="30"></li>
-      <li><h1 style="margin: 0">${name.official || "No name"}</h1></li>
-    </ul>
-    <p><b>Capital:</b> ${capital || "No capital"}</p>
-    <p><b>Population:</b> ${population || "No population"}</p>
-    <p><b>Languages:</b> ${(Object.values(languages)).join(' ') || "No languages"}</p>`;
+  <div class="photo-card">
+  <img src="${webformatURL}" alt="${tags}" loading="lazy" />
+  <div class="info">
+    <p class="info-item">
+      <b>Likes</b><br>${likes}
+    </p>
+    <p class="info-item">
+      <b>Views</b><br>${views}
+    </p>
+    <p class="info-item">
+      <b>Comments</b><br>${comments}</b>
+    </p>
+    <p class="info-item">
+      <b>Downloads</b><br>${downloads}</b>
+    </p>
+  </div>
+</div>`;
 };
 
-function createMarkupList({ name, flags }) {  
-  return `
-  <li style="list-style: none"><ul style="display: flex; align-items: baseline; margin: 0; padding: 0; gap: 10px">
-    <li><img src=${flags.svg || "No flags"} width="30"></li>
-    <li><h2 style="margin: 0">${name.official || "No name"}</h2></li>
-  </ul></li>`;
-};
-
-function updateNewsInfo(markup) {
-  refs.countryList.innerHTML = "";
-  refs.countryInfo.innerHTML = markup;
-};
-
-function updateNewsList(markup) {
-  refs.countryInfo.innerHTML = "";
-  refs.countryList.innerHTML = markup;
+function updateGalleryList(markup) {
+  refs.galleryList.innerHTML = markup;
+  console.log(refs.galleryList);
 };
 
 function onError(err) {
-  Notiflix.Notify.failure('Oops, there is no country with that name');
-  refs.countryInfo.innerHTML = "";
-  refs.countryList.innerHTML = "";
+  Notiflix.Notify.failure('Sorry, there are no images matching your search query. Please try again');  
+  refs.galleryList.innerHTML = "";
 };
